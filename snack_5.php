987fb7180768f41ff8a9db67a1219afa430a6e91
pierfrancescoscar <scarinci401@gmail.com>
2021-12-17 16:13:22 +0100

    ADD: snack 5 complete

--- snack_5.php
+++ snack_5.php
@@ -2,7 +2,9 @@
 Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
 
 <?php
-    $paragraph = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et officia excepturi totam praesentium iusto sapiente, nam quidem libero doloremque error accusantium fugiat, eius autem ex molestiae recusandae incidunt velit dicta.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut qui expedita ipsum voluptate laborum. Quod sed consequuntur ea, totam pariatur, laborum vero iste quasi quam illo sapiente quisquam incidunt tempora.';
+    echo nl2br('Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+    Et officia excepturi totam praesentium iusto sapiente, nam quidem libero doloremque error accusantium fugiat, eius autem ex molestiae recusandae incidunt velit dicta.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Aut qui expedita ipsum voluptate laborum. Quod sed consequuntur ea, totam pariatur, laborum vero iste quasi quam illo sapiente quisquam incidunt tempora.');
 
 ?>
