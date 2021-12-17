e726dc3146a9c954a3c42e8ede561c93335dbafb
pierfrancescoscar <scarinci401@gmail.com>
2021-12-17 14:59:44 +0100

    ADD: general setup

---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+Qui l'array di esempio: https://www.codepile.net/pile/R2K5d68z
+
+Snack 4
+Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta
+
+Snack 5
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+
+Snack 6
+Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
+
+Snack 7
+Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<!-- -- SVOLGIMENTO --  -->
+
+<!-- 1. Snack -->
+
+<?php
+
+
+?>
