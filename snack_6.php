6783974fb2f251c6ad6e4b022180460a62c987d2
pierfrancescoscar <scarinci401@gmail.com>
2021-12-17 16:34:30 +0100

    ADD: snack 6 keys setup

---
+++ snack_6.php
@@ -0,0 +1,34 @@
+<!-- Snack 6
+Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
+
+<?php
+ 
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+
+
+    $dbKeys = array_keys($db);
+    var_dump($dbKeys);
+
+ 
+?>
