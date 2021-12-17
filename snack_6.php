360706aebddb087dbcf7a273fd1bdc65d88f2cd8
pierfrancescoscar <scarinci401@gmail.com>
2021-12-17 16:47:17 +0100

    ADD: complete snack 6

--- snack_6.php
+++ snack_6.php
@@ -32,3 +32,48 @@ Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro arra
 
  
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snack 6</title>
+</head>
+<body>
+
+    <div style="background-color: grey;" class="teachers">
+
+    <h2>
+        <?php echo $dbKeys[0] ?>
+    </h2>
+
+        <p>
+            <?php
+                for ($i = 0; $i < count($db[$dbKeys[0]]); $i++) {
+                    echo $db[$dbKeys[0]][$i]['name'].$db[$dbKeys[0]][$i]['lastname'].'<br>'; 
+                }
+            ?>
+        </p>
+
+    </div>
+
+    <div style="background-color: green;" class="pm">
+        <h2>
+            <?php echo $dbKeys[1] ?>
+        </h2>
+
+        <p>
+            <?php
+                for ($i = 0; $i < count($db[$dbKeys[1]]); $i++) {
+                    echo $db[$dbKeys[1]][$i]['name'].$db[$dbKeys[0]][$i]['lastname'].'<br>';
+                }
+            ?>
+        </p>
+
+    </div>
+    
+</body>
+</html>
+
